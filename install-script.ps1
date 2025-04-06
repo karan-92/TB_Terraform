d9b3fb3644ec246e22d0bd06c94fa6dc30b187a7
karan-92 <karan.kapildewsing@testbotics.nl>
2025-04-06 14:24:17 +0200

    Update install-script.ps1

--- install-script.ps1
+++ install-script.ps1
@@ -1,43 +1,19 @@
 # install-script.ps1
 
-# Zet execution policy en beveiliging
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Download en installeer Visual Studio Code (User installer)
-$vsInstaller = "$env:TEMP\\vscode-setup.exe"
-Invoke-WebRequest -Uri "https://update.code.visualstudio.com/latest/win32-x64-user/stable" -OutFile $vsInstaller
-Start-Process -FilePath $vsInstaller -ArgumentList '/silent', '/mergetasks=!runcode' -Wait
+# Installeer Python, Git en Google Chrome
+choco install -y python git googlechrome
 
-# Download en installeer Python
-$pythonInstaller = "$env:TEMP\\python-installer.exe"
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.11.7/python-3.11.7-amd64.exe" -OutFile $pythonInstaller
-Start-Process -FilePath $pythonInstaller -ArgumentList "/quiet InstallAllUsers=0 PrependPath=1 Include_test=0" -Wait
+# Voeg pip scripts toe aan PATH
+$env:Path += ";C:\Python311\Scripts"
 
-# Voeg Scripts folder toe aan PATH voor deze sessie
-$env:Path += ";$env:LOCALAPPDATA\\Programs\\Python\\Python311\\Scripts"
-
-# Installeer Robot Framework tooling
+# Installeer Robot Framework + Selenium
 pip install -U pip
 pip install robotframework
-pip install robotframework-browser
-rfbrowser init
+pip install robotframework-seleniumlibrary
 
-# VS Code extensies installeren (RobotCode + Python)
-$codeCmd = \"$env:LOCALAPPDATA\\Programs\\Microsoft VS Code\\bin\\code\"
-& $codeCmd --install-extension daniel-biehl.robotcode
-& $codeCmd --install-extension ms-python.python
-& $codeCmd --install-extension ms-python.vscode-pylance
-
-# VS Code settings.json configureren
-$settingsPath = \"$env:APPDATA\\Code\\User\\settings.json\"
-
-@"
-{
-  \"python.defaultInterpreterPath\": \"C:\\\\Users\\\\$env:USERNAME\\\\AppData\\\\Local\\\\Programs\\\\Python\\\\Python311\\\\python.exe\",
-  \"python.languageServer\": \"Pylance\",
-  \"robotcode.python.interpreter\": \"C:\\\\Users\\\\$env:USERNAME\\\\AppData\\\\Local\\\\Programs\\\\Python\\\\Python311\\\\python.exe\",
-  \"robotcode.robotframework.libraries.builtin\": true,
-  \"robotcode.robotframework.libraries.thirdparty\": true
-}
-"@ | Out-File -Encoding UTF8 -FilePath $settingsPath -Force
+# Download en installeer ChromeDriver (versie afgestemd op geïnstalleerde Chrome)
+choco install -y chromedriver --ignore-checksums
